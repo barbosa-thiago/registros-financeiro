b9eef7f984d1c75fbe76edcb2264b76c9ed4a8ee
Thiago <thiago.barbosa@nkey.digital>
2023-11-10 10:31:00 -0300

    refactor: nome da aplicacao

--- src/main/java/com/example/sintegracebusca/SintegraBuscaApplication.java
+++ src/main/java/com/example/sintegracebusca/SintegraBuscaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoPhotoApplication {
+public class SintegraBuscaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoPhotoApplication.class, args);
+        SpringApplication.run(SintegraBuscaApplication.class, args);
     }
 
 }
