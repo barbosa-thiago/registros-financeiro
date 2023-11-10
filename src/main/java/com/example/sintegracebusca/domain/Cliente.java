08026d51b19ea3ad8e61dc65a5fbc701350d3c6f
Thiago <thiago.barbosa@nkey.digital>
2023-11-10 10:26:41 -0300

    fix: adicionando campos Cliente

--- src/main/java/com/example/sintegracebusca/domain/Cliente.java
+++ src/main/java/com/example/sintegracebusca/domain/Cliente.java
@@ -20,4 +20,34 @@ public class Cliente implements Serializable {
 
     @Column(name = "cnpj", unique = true)
     String cnpj;
+
+    @Column(name = "razao_social")
+    String razaoSocial;
+
+    @Column(name = "logradouro")
+    String logradouro;
+
+    @Column(name = "numero")
+    String numero;
+
+    @Column(name = "complemento")
+    String complemento;
+
+    @Column(name = "bairro")
+    String bairro;
+
+    @Column(name = "cep")
+    String cep;
+
+    @Column(name = "uf")
+    String uf;
+
+    @Column(name = "municipio")
+    String municipio;
+
+    @Column(name = "inscricao_estadual")
+    String inscricaoEstadual;
+
+    @Column(name = "telefone")
+    String telefone;
 }
