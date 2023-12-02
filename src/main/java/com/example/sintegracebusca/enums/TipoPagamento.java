05774b6b8d179a85f249a82b10479cfebf796799
Thiago <thiago.barbosa@nkey.digital>
2023-12-02 17:09:39 -0300

    feat: templates

---
+++ src/main/java/com/example/sintegracebusca/enums/TipoPagamento.java
@@ -0,0 +1,12 @@
+package com.example.sintegracebusca.enums;
+
+public enum TipoPagamento {
+    FORNECEDOR,
+    CONTA_LUZ,
+    ALUGUEL,
+    IMPOSTO,
+    TRANSPORTE,
+    ALIMENTACAO,
+    OUTROS,
+    AGENDADO
+}
