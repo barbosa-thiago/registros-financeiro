0ec007bfa02150fe21a2da71eaf8a597965b329b
Thiago <thiago.barbosa@nkey.digital>
2023-12-12 16:14:53 -0300

    feat: refactor

--- src/main/java/com/example/sintegracebusca/controller/PagamentoController.java
+++ src/main/java/com/example/sintegracebusca/controller/PagamentoController.java
@@ -1,8 +1,6 @@
 package com.example.sintegracebusca.controller;
 
-import com.example.sintegracebusca.domain.Agendamento;
 import com.example.sintegracebusca.domain.Pagamento;
-import com.example.sintegracebusca.enums.TipoPagamento;
 import com.example.sintegracebusca.service.AgendamentoService;
 import com.example.sintegracebusca.service.CompraService;
 import com.example.sintegracebusca.service.PagamentoService;
@@ -12,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 import static java.util.Objects.isNull;
 
@@ -34,33 +30,12 @@ public class PagamentoController {
 
 
     @PostMapping("/pagamentos")
-    public String save(@ModelAttribute Pagamento pagamento, BindingResult bindingResult, Model model) {
+    public String save(@RequestBody Pagamento pagamento, BindingResult bindingResult, Model model) {
         var pagamentoSalvo = pagamentoService.save(pagamento);
         if(isNull(pagamentoSalvo))
             return "erro";
         model.addAttribute("pagamento", pagamentoSalvo);
         return "pagamento/pagamentoResult";
-    }
-
-    @PostMapping("/pagamentos/agendado/{agendamentoId}")
-    public String saveAgendado(@PathVariable Long agendamentoId,
-                               @ModelAttribute Agendamento agendamento,
-                               BindingResult bindingResult,
-                               Model model) {
-        var agendamentoUpdate = agendamentoService.update(agendamentoId, !agendamento.getPago());
-
-        var pagamento = Pagamento.builder()
-            .dataPagamento(LocalDate.now())
-            .tipo(TipoPagamento.AGENDADO)
-            .valor(agendamentoUpdate.getValor())
-            .agendamento(agendamentoUpdate)
-            .descricao(agendamentoUpdate.getFornecedor())
-            .build();
-
-        var pagamentoSalvo = pagamentoService.save(pagamento);
-        if(isNull(pagamentoSalvo))
-            return "erro";
-        return "redirect:/agendamentos/list";
     }
 
     @GetMapping("pagamentos/list")
